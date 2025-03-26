8618c5a454d7749b205cf18a2920a9a61e2075ed
any <any>
2025-03-26 21:09:58 +0800

    update dependency, add execute permission for dist/index.js

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { server } from "./mcp/server.js";
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+export async function start() {
+    await server.connect(transport);
+}
+start();
